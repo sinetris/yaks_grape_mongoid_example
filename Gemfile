4de0bba118f50c88fab6c98eebd797328524f90a
Duilio Ruggiero <duilio.ruggiero@gmail.com>
2014-07-23 19:49:34 +0200

    add mongoid

--- Gemfile
+++ Gemfile
@@ -2,12 +2,17 @@ source 'https://rubygems.org'
 
 gem 'rack'
 gem 'pry'
+gem "mongoid", "~> 4.0.0"
+gem 'bcrypt'
 
 group :development, :test do
   gem 'rack-test'
+  gem "factory_girl", "~> 4.0"
 end
 
 group :test do
   gem 'rspec', '~> 3.0'
   gem 'capybara'
+  gem 'database_cleaner'
+  gem 'mongoid-rspec', github: 'pcreux/mongoid-rspec', branch: 'mongo-4-rspec-3'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,13 +2,18 @@ ENV["RACK_ENV"] = 'test'
 require File.join(File.dirname(__FILE__), '..', 'config/environment')
 require 'rspec'
 require 'capybara/rspec'
+require 'database_cleaner'
+require 'factory_girl'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[File.expand_path('../support/**/*.rb"', __FILE__)].each { |f| require f }
 
+FactoryGirl.find_definitions
+
 RSpec.configure do |config|
   config.include Rack::Test::Methods
+  config.include Mongoid::Matchers
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
