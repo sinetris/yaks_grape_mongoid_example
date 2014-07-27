942bb002275e6ac2272dba33d2f11ff0d3fb492f
Duilio Ruggiero <duilio.ruggiero@gmail.com>
2014-07-27 17:05:16 +0200

    fix typo in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,7 @@ require 'factory_girl'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[File.expand_path('../support/**/*.rb"', __FILE__)].each { |f| require f }
+Dir[File.expand_path('../support/**/*.rb', __FILE__)].each { |f| require f}
 
 FactoryGirl.find_definitions
 
